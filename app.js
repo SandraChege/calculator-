054d4406f3d7f9adaa312c7cba16db05a9871627
SandraChege <chegemwihaki88@gmail.com>
2023-10-13 12:15:04 +0300

    Corrects the functionality of the calculator

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector(".calculator_btns");
 const display = document.querySelector('.calculator_display')
-const previousKeyType = calculator.dataset.previousKeyType;
+// const previousKeyType = calculator.dataset.previousKeyType;
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
@@ -14,6 +14,7 @@ keys.addEventListener('click', e => {
         Array.from(key.parentNode.children)
         .forEach(k => k.classList.remove('is-depressed'))
 
+        const previousKeyType = calculator.dataset.previousKeyType;
         if (!action) {
           if (displayedNum === "0" || previousKeyType === "operator") {
             display.textContent = keyContent;
